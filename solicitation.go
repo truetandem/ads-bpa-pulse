618cdc3150943c414bd5b3083135be6791b0d74b
Bryan Allred <bryan.allred@gmail.com>
2016-05-17 15:56:51 -0400

    TG-7 added Get() and Save() for solicitations

--- solicitation.go
+++ solicitation.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/json"
 	"fmt"
 	"time"
+
+	"google.golang.org/appengine/datastore"
+
+	"golang.org/x/net/context"
 )
 
 // Solicitation request for a bid.
 type Solicitation struct {
-	Title      string            `json:"title"`
-	Properties map[string]string `json:"properties"`
-	Modified   time.Time         `json:"modified"`
+	Title      string            `json:"title" datastore:"Title"`
+	Properties map[string]string `json:"properties" datastore:"-"`
+	Modified   time.Time         `json:"modified" datastore:"Modified,noindex"`
+	checksum   string            `datastore:"Checksum"`
+	properties []byte            `datastore:"Properties"`
 }
 
 // ToString returns the Solicitation as a formatted string.
@@ -26,4 +33,45 @@ func (s *Solicitation) ToString() string {
 // Checksum returns the SHA-1 checksum of the Solicitation.
 func (s *Solicitation) Checksum() string {
 	return fmt.Sprintf("%x", sha1.Sum([]byte(s.ToString())))
+}
+
+// Get a Solicitation from the datastore.
+func (s *Solicitation) Get(ctx context.Context) (Solicitation, error) {
+	// Create a new key based on the title and attempt get the
+	// entity
+	var sol Solicitation
+	key := datastore.NewKey(ctx, "Solicitation", s.Title, 0, nil)
+	if err := datastore.Get(ctx, key, sol); err != nil {
+		return sol, err
+	}
+
+	// Unmarshal the binary JSON and store it as a map
+	if err := json.Unmarshal(sol.properties, sol.Properties); err != nil {
+		return sol, err
+	}
+
+	return sol, nil
+}
+
+// Save a Solicitation to the datastore.
+func (s *Solicitation) Save(ctx context.Context) error {
+	// Save the checksum to an internal variable for storage
+	// purposes
+	s.checksum = s.Checksum()
+
+	// Marshal the property map so we can save it as a binary
+	// field
+	js, err := json.Marshal(s.Properties)
+	if err != nil {
+		return err
+	}
+	s.properties = js
+
+	// Create the key based on the title and store it
+	key := datastore.NewKey(ctx, "Solicitation", s.Title, 0, nil)
+	if _, err := datastore.Put(ctx, key, s); err != nil {
+		return err
+	}
+
+	return nil
 }
--- solicitation_test.go
+++ solicitation_test.go
@@ -1,15 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"google.golang.org/appengine/aetest"
+)
 
 func TestSolicitationToString(t *testing.T) {
+	expected := "Title: Test\n\n  - ID: TBD"
 	s := Solicitation{
 		Title: "Test",
 		Properties: map[string]string{
 			"ID": "TBD",
 		},
 	}
-	expected := "Title: Test\n\n  - ID: TBD"
 
 	if s.ToString() != expected {
 		t.FailNow()
@@ -17,15 +21,55 @@ func TestSolicitationToString(t *testing.T) {
 }
 
 func TestSolicitationChecksum(t *testing.T) {
+	expected := "f866a997e7b28e85a438e7415908e4f51edaf43e"
 	s := Solicitation{
 		Title: "Test",
 		Properties: map[string]string{
 			"ID": "TBD",
 		},
 	}
-	expected := "f866a997e7b28e85a438e7415908e4f51edaf43e"
 
 	if s.Checksum() != expected {
 		t.FailNow()
+	}
+}
+
+func TestSolicitationGet(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer done()
+
+	s := Solicitation{
+		Title: "Test",
+		Properties: map[string]string{
+			"ID": "TBD",
+		},
+	}
+
+	_, err = s.Get(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSolicitationSave(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer done()
+
+	s := Solicitation{
+		Title: "Test",
+		Properties: map[string]string{
+			"ID": "TBD",
+		},
+	}
+
+	err = s.Save(ctx)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
